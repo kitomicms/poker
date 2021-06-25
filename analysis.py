a9fa1452bd67f01cd1794979c51953ebcae1b9ed
kitomicms <chunkit721@gmail.com>
2021-06-25 16:40:14 +0800

    Create analysis.py

---
+++ analysis.py
@@ -0,0 +1,15 @@
+# analyze different %
+# all the combination of hand
+# combination vs permutation
+
+
+
+from Deck import *
+
+# Create a deck
+deck = Deck()
+
+## winning odd when you only know your hand
+
+## the combination of two cards
+## permutation
